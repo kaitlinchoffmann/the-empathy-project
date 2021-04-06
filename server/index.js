ad244fdc568825225430382d5293453b01763b3d
Kaitlin <kaitlinchoffmann@gmail.com>
2021-04-06 11:18:03 -0400

    update dist

--- server/index.js
+++ server/index.js
@@ -35,6 +35,10 @@ app.use(function(req, res, next) {
 app.use("/", questionRoutes);
 app.use('/answers', answerRoutes);
 
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../client/views', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
